af07935e1532e20130a51e3c18bedd08978bae2b
Sally E <sme418@nyu.edu>
2022-11-03 08:17:27 -0400

    initialized route

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+});
+
+// define the index page route
+router.get('/', (req, res) => {
+    res.send('Wow!')
+});
+
+module.exports = router;
