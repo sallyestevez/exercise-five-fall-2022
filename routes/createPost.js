5b7ca5112cf213d3034f047a59a22d36c80c0925
Sally E <sme418@nyu.edu>
2022-11-10 08:40:27 -0500

    Add posts to firebase function

--- routes/createPost.js
+++ routes/createPost.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
+// initialize firestore - cloud database
+const firestore = require("firebase/firestore");
+// Initialize Cloud Firestore and get a reference to the service
+const db = firestore.getFirestore();
 
+// action will be URL
 const createPostForm = `
 <h1>Create Post</h1>
-<form action="">
-    <input type="text" name="title" placeholder="Title" />
-    <input type="text" name="text" placeholder="Text" />
-    <input type="text" name="author" placeholder="Author" />
-    <button type="submit">Submit</button>
+<form action="/create/submit">
+    <div style="display: flex; flex-direction: column; max-width: 400px;">
+        <label for="title">Title</label>
+        <input type="text" name="postTitle" placeholder="Title" />
+        <label for="text">Text</label>
+        <input type="text" name="postText" placeholder="Text" />
+        <label for="author">Author</label>
+        <input type="text" name="postAuthor" placeholder="Author" />
+        <button type="submit">Submit</button>
+    </div>
 </form>
 `;
 
@@ -19,6 +29,43 @@ router.use((req, res, next) => {
 
 router.get("/", (req, res) => {
     res.send(createPostForm);
+});
+
+router.get("/submit", (req, res) => {
+    const queryParams = req.query; // query params from URL
+    const title = queryParams.postTitle;
+    const text = queryParams.postText;
+    const author = queryParams.postAuthor;
+
+    // create ID from title - using regex to replace characters
+    // allows it to work as URL
+    // replacing all spaces with dashes sand making text lowercase
+    const idFromTitle = title.replace(/\s+/g, "-").toLowerCase();
+
+    // submit post to Firebase
+    const setBlogPost = firestore.setDoc(
+        firestore.doc(db, "posts", idFromTitle),
+        {
+            title: title,
+            text: text,
+            author: author,
+        }
+    );
+
+    setBlogPost
+        .then((response) => {
+            res.send(`
+                <h1>Submission Successful!</h1>
+                <p><a href="/create">Add another post</p>
+            `);
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
+    
+    
+    res.send({title, text, author});
 });
 
 module.exports = router;
